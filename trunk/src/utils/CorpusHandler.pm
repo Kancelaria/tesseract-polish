1c53399caf5cd300f1443a6646af53a94358670b
aleksander.adamowski <aleksander.adamowski@22d5682e-f051-0410-a907-a9ad6036efb7>
2008-07-14 15:16:59 +0000

    Added licensing information in the sources

--- trunk/src/utils/CorpusHandler.pm
+++ trunk/src/utils/CorpusHandler.pm
@@ -1,7 +1,17 @@
-# This module handles parsing of XCES-coded text corpus
-# and updating of word occurence count in a hash.
 
 package CorpusHandler;
+
+=head1 NAME
+
+CorpusHandler - SAX parser handler for counting occurences of words in a XCES 
+text corpus
+
+=head1 DESCRIPTION
+
+ This module handles parsing of XCES-coded text corpus
+ and updating of word occurence count in a hash.
+
+=cut
 
 use Data::Dumper;
 use encoding 'utf8';
@@ -49,5 +59,24 @@ sub characters {
 sub end_document {
 	print "End document\n";
 }
+
+=head1 COPYRIGHT
+
+ Copyright 2009 Aleksander Adamowski
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+		 http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+
+=cut
+
 
 1;
